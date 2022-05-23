c829d01cad04d2dd6938fb623d84823e9f423116
NIKITA Vorobyev <73196137+neketskk@users.noreply.github.com>
2022-05-24 02:31:21 +0300

    test2

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,21 +4,21 @@
 
 
 TEST(task1, test1) {
-	Shop shop(7, 77, 177, 3, 7);
+	Shop shop(0, 70, 500, 4, 7);
 	shop.Work();
 	int result = shop.servedClients;
 	EXPECT_EQ(0, result);
 }
 
 TEST(task2, test2) {
-	Shop shop(0, 43, 123, 6, 3);
+	Shop shop(9, 100, 400, 4, 7);
 	shop.Work();
 	int result = shop.rejectedClients;
 	EXPECT_EQ(0, result);
 }
 
 TEST(task3, test3) {
-	Shop shop(4, 44, 333, 5, 2);
+	Shop shop(1, 70, 333, 7, 3);
 	shop.Work();
 	int result = 0;
 	if (shop.rejectedClients > 30)
