49c24d9a80e221d9cdc8c3f2a8ff3a9e33893b4c
NIKITA Vorobyev <73196137+neketskk@users.noreply.github.com>
2022-05-24 02:26:03 +0300

    t1

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,27 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2022 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "../include/task.h"
+
+
+TEST(task1, test1) {
+	Shop shop(7, 77, 177, 3, 7);
+	shop.Work();
+	int result = shop.servedClients;
+	EXPECT_EQ(0, result);
+}
+
+TEST(task2, test2) {
+	Shop shop(0, 43, 123, 6, 3);
+	shop.Work();
+	int result = shop.rejectedClients;
+	EXPECT_EQ(0, result);
+}
+
+TEST(task3, test3) {
+	Shop shop(4, 44, 333, 5, 2);
+	shop.Work();
+	int result = 0;
+	if (shop.rejectedClients > 30)
+		result = 1;
+	EXPECT_EQ(1, result);
+}
